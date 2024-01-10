38acb78f30bb2255c4ee8f491fbe841447f89f0c
umutkozak <147056208+umutozak@users.noreply.github.com>
2024-01-10 22:30:04 +0300

    userAdded

--- AvangersDietApp.UI/Main.cs
+++ AvangersDietApp.UI/Main.cs
@@ -37,9 +37,49 @@ namespace AvangersDietApp.UI
             else
             {
                 AdminOperations adminOperations = new AdminOperations();
-                adminOperations.ShowDialog();
-                this.Close();
+                adminOperations.Show();
+                this.Hide();
             }
+
+        }
+
+        private void btn_Uye_Click(object sender, EventArgs e)
+        {
+            User user = new User();
+
+            user.UserName=txt_User.Text;
+            user.Password=msk_UserPass.Text;
+
+            if (string.IsNullOrWhiteSpace(txt_User.Text)||string.IsNullOrWhiteSpace(msk_UserPass.Text))
+            {
+                MessageBox.Show("Lütfen yönetici adý ve þifrenizi giriniz.");
+                txt_User.Text= "";
+                msk_UserPass.Text = "";
+            }
+            else if (user.UserName!=txt_User.Text)
+            {
+                MessageBox.Show("Yönetici adýnýz hatalý! Lütfen tekrar giriniz.");
+                txt_User.Text = "";
+                msk_UserPass.Text = "";
+            }
+            else if (user.Password!= msk_UserPass.Text)
+            {
+                MessageBox.Show("Þifreniz hatalý! Lütfen tekrar giriniz.");
+                txt_User.Text = "";
+                msk_UserPass.Text = "";
+            }
+            else
+            {
+
+            }
+        }
+
+        private void Btn_UyeOl_Click(object sender, EventArgs e)
+        {
+            
+            Register registerForm = new Register();
+            registerForm.Show();
+            this.Hide();
 
         }
     }
