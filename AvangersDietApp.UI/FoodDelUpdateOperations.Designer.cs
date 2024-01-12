1a06326a55bcaae5b0edad5a5a130ce4cd3bf5a3
umutkozak <147056208+umutozak@users.noreply.github.com>
2024-01-12 14:43:51 +0300

    AdminCategoryUpdated

--- AvangersDietApp.UI/FoodDelUpdateOperations.Designer.cs
+++ AvangersDietApp.UI/FoodDelUpdateOperations.Designer.cs
@@ -28,12 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FoodDelUpdateOperations";
+            dataGridView1 = new DataGridView();
+            button1 = new Button();
+            button2 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 12);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(772, 334);
+            dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            button1.Location = new Point(27, 367);
+            button1.Name = "button1";
+            button1.Size = new Size(316, 52);
+            button1.TabIndex = 1;
+            button1.Text = "Yemek Güncelle";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            button2.Location = new Point(396, 367);
+            button2.Name = "button2";
+            button2.Size = new Size(325, 52);
+            button2.TabIndex = 1;
+            button2.Text = "Yemek Sil";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // FoodDelUpdateOperations
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(dataGridView1);
+            Name = "FoodDelUpdateOperations";
+            Text = "FoodDelUpdateOperations";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private DataGridView dataGridView1;
+        private Button button1;
+        private Button button2;
     }
 }
