d4a7c50d00cb5edec6203d814c68e3042eb10e70
altaniket <ialtantekin@gmail.com>
2024-01-12 13:11:49 +0300

    userFoodDairy

---
+++ AvangersDietApp.UI/UserFoodDairy.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace AvangersDietApp.UI
+{
+    partial class UserFoodDairy
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            comboBox3 = new ComboBox();
+            label6 = new Label();
+            comboBox4 = new ComboBox();
+            dateTimePicker1 = new DateTimePicker();
+            dataGridView1 = new DataGridView();
+            label7 = new Label();
+            btn_Ekle = new Button();
+            btnKaydet = new Button();
+            btnSil = new Button();
+            numericUpDown1 = new NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Verdana", 12F);
+            label1.Location = new Point(53, 138);
+            label1.Name = "label1";
+            label1.Size = new Size(51, 18);
+            label1.TabIndex = 0;
+            label1.Text = "Öğün";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Verdana", 12F);
+            label2.Location = new Point(53, 189);
+            label2.Name = "label2";
+            label2.Size = new Size(77, 18);
+            label2.TabIndex = 0;
+            label2.Text = "Kategori";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Verdana", 12F);
+            label3.Location = new Point(53, 237);
+            label3.Name = "label3";
+            label3.Size = new Size(83, 18);
+            label3.TabIndex = 0;
+            label3.Text = "Yemekler";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Verdana", 12F);
+            label4.Location = new Point(53, 295);
+            label4.Name = "label4";
+            label4.Size = new Size(77, 18);
+            label4.TabIndex = 0;
+            label4.Text = "Porsiyon";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Verdana", 12F);
+            label5.Location = new Point(53, 356);
+            label5.Name = "label5";
+            label5.Size = new Size(46, 18);
+            label5.TabIndex = 0;
+            label5.Text = "Adet";
+            // 
+            // comboBox1
+            // 
+            comboBox1.Font = new Font("Verdana", 12F);
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(185, 186);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(213, 26);
+            comboBox1.TabIndex = 1;
+            // 
+            // comboBox2
+            // 
+            comboBox2.Font = new Font("Verdana", 12F);
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(185, 237);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(213, 26);
+            comboBox2.TabIndex = 1;
+            // 
+            // comboBox3
+            // 
+            comboBox3.Font = new Font("Verdana", 12F);
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Location = new Point(185, 292);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(213, 26);
+            comboBox3.TabIndex = 1;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Verdana", 12F);
+            label6.Location = new Point(53, 97);
+            label6.Name = "label6";
+            label6.Size = new Size(47, 18);
+            label6.TabIndex = 0;
+            label6.Text = "Tarih";
+            // 
+            // comboBox4
+            // 
+            comboBox4.Font = new Font("Verdana", 12F);
+            comboBox4.FormattingEnabled = true;
+            comboBox4.Location = new Point(185, 135);
+            comboBox4.Name = "comboBox4";
+            comboBox4.Size = new Size(213, 26);
+            comboBox4.TabIndex = 1;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Font = new Font("Verdana", 12F);
+            dateTimePicker1.Location = new Point(157, 94);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(266, 27);
+            dateTimePicker1.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(476, 186);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(251, 170);
+            dataGridView1.TabIndex = 3;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(476, 159);
+            label7.Name = "label7";
+            label7.Size = new Size(57, 15);
+            label7.TabIndex = 4;
+            label7.Text = "Seçilenler";
+            // 
+            // btn_Ekle
+            // 
+            btn_Ekle.Location = new Point(185, 390);
+            btn_Ekle.Name = "btn_Ekle";
+            btn_Ekle.Size = new Size(75, 23);
+            btn_Ekle.TabIndex = 5;
+            btn_Ekle.Text = "Ekle";
+            btn_Ekle.UseVisualStyleBackColor = true;
+            // 
+            // btnKaydet
+            // 
+            btnKaydet.Location = new Point(476, 390);
+            btnKaydet.Name = "btnKaydet";
+            btnKaydet.Size = new Size(75, 23);
+            btnKaydet.TabIndex = 5;
+            btnKaydet.Text = "Kaydet";
+            btnKaydet.UseVisualStyleBackColor = true;
+            // 
+            // btnSil
+            // 
+            btnSil.Location = new Point(652, 390);
+            btnSil.Name = "btnSil";
+            btnSil.Size = new Size(75, 23);
+            btnSil.TabIndex = 5;
+            btnSil.Text = "Sil";
+            btnSil.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(185, 351);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(213, 23);
+            numericUpDown1.TabIndex = 6;
+            // 
+            // UserFoodDairy
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(numericUpDown1);
+            Controls.Add(btnSil);
+            Controls.Add(btnKaydet);
+            Controls.Add(btn_Ekle);
+            Controls.Add(label7);
+            Controls.Add(dataGridView1);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(comboBox3);
+            Controls.Add(comboBox2);
+            Controls.Add(comboBox4);
+            Controls.Add(comboBox1);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label6);
+            Controls.Add(label1);
+            Name = "UserFoodDairy";
+            Text = "UserFoodDairy";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private ComboBox comboBox3;
+        private Label label6;
+        private ComboBox comboBox4;
+        private DateTimePicker dateTimePicker1;
+        private DataGridView dataGridView1;
+        private Label label7;
+        private Button btn_Ekle;
+        private Button btnKaydet;
+        private Button btnSil;
+        private NumericUpDown numericUpDown1;
+    }
+}
