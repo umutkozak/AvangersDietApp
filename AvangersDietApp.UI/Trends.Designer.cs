1d4d822767bf07d350e0eff8b8b2dbeaf1e8427f
Batuhan ÖZYILMAZ <ozylmz.batuhan@gmail.com>
2024-01-17 19:15:07 +0300

    Trends formu eklendi

---
+++ AvangersDietApp.UI/Trends.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace AvangersDietApp.UI
+{
+    partial class Trends
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblName = new Label();
+            label1 = new Label();
+            lblCurrentUserBreakfast = new Label();
+            lblCurrentUserLunch = new Label();
+            lblCurrentUserDinner = new Label();
+            lblCurrentUserSnack = new Label();
+            lblAllUserSnack = new Label();
+            lblAllUserDinner = new Label();
+            lblAllUserLunch = new Label();
+            lblAllUserBreakfast = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            btnMothly = new Label();
+            btnWeekly = new Label();
+            pictureBox1 = new PictureBox();
+            lstTop5 = new ListBox();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // lblName
+            // 
+            lblName.BackColor = Color.LightCoral;
+            lblName.Font = new Font("Verdana", 12F, FontStyle.Bold);
+            lblName.Location = new Point(31, 210);
+            lblName.Name = "lblName";
+            lblName.Size = new Size(102, 129);
+            lblName.TabIndex = 0;
+            lblName.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            label1.BackColor = Color.LightCoral;
+            label1.Font = new Font("Verdana", 12F, FontStyle.Bold);
+            label1.Location = new Point(31, 370);
+            label1.Name = "label1";
+            label1.Size = new Size(102, 129);
+            label1.TabIndex = 1;
+            label1.Text = "Diğer Kullanıcılar";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lblCurrentUserBreakfast
+            // 
+            lblCurrentUserBreakfast.BackColor = Color.LightSalmon;
+            lblCurrentUserBreakfast.Font = new Font("Verdana", 12F, FontStyle.Bold);
+            lblCurrentUserBreakfast.Location = new Point(164, 210);
+            lblCurrentUserBreakfast.Name = "lblCurrentUserBreakfast";
+            lblCurrentUserBreakfast.Size = new Size(209, 129);
+            lblCurrentUserBreakfast.TabIndex = 2;
+            lblCurrentUserBreakfast.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lblCurrentUserLunch
+            // 
+            lblCurrentUserLunch.BackColor = Color.LightSalmon;
+            lblCurrentUserLunch.Font = new Font("Verdana", 12F, FontStyle.Bold);
+            lblCurrentUserLunch.Location = new Point(390, 210);
+            lblCurrentUserLunch.Name = "lblCurrentUserLunch";
+            lblCurrentUserLunch.Size = new Size(209, 129);
+            lblCurrentUserLunch.TabIndex = 3;
+            lblCurrentUserLunch.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lblCurrentUserDinner
+            // 
+            lblCurrentUserDinner.BackColor = Color.LightSalmon;
+            lblCurrentUserDinner.Font = new Font("Verdana", 12F, FontStyle.Bold);
+            lblCurrentUserDinner.Location = new Point(616, 210);
+            lblCurrentUserDinner.Name = "lblCurrentUserDinner";
+            lblCurrentUserDinner.Size = new Size(209, 129);
+            lblCurrentUserDinner.TabIndex = 4;
+            lblCurrentUserDinner.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lblCurrentUserSnack
+            // 
+            lblCurrentUserSnack.BackColor = Color.LightSalmon;
+            lblCurrentUserSnack.Font = new Font("Verdana", 12F, FontStyle.Bold);
+            lblCurrentUserSnack.Location = new Point(844, 210);
+            lblCurrentUserSnack.Name = "lblCurrentUserSnack";
+            lblCurrentUserSnack.Size = new Size(209, 129);
+            lblCurrentUserSnack.TabIndex = 5;
+            lblCurrentUserSnack.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lblAllUserSnack
+            // 
+            lblAllUserSnack.BackColor = Color.LightSalmon;
+            lblAllUserSnack.Font = new Font("Verdana", 12F, FontStyle.Bold);
+            lblAllUserSnack.Location = new Point(844, 370);
+            lblAllUserSnack.Name = "lblAllUserSnack";
+            lblAllUserSnack.Size = new Size(209, 129);
+            lblAllUserSnack.TabIndex = 9;
+            lblAllUserSnack.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lblAllUserDinner
+            // 
+            lblAllUserDinner.BackColor = Color.LightSalmon;
+            lblAllUserDinner.Font = new Font("Verdana", 12F, FontStyle.Bold);
+            lblAllUserDinner.Location = new Point(616, 370);
+            lblAllUserDinner.Name = "lblAllUserDinner";
+            lblAllUserDinner.Size = new Size(209, 129);
+            lblAllUserDinner.TabIndex = 8;
+            lblAllUserDinner.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lblAllUserLunch
+            // 
+            lblAllUserLunch.BackColor = Color.LightSalmon;
+            lblAllUserLunch.Font = new Font("Verdana", 12F, FontStyle.Bold);
+            lblAllUserLunch.Location = new Point(390, 370);
+            lblAllUserLunch.Name = "lblAllUserLunch";
+            lblAllUserLunch.Size = new Size(209, 129);
+            lblAllUserLunch.TabIndex = 7;
+            lblAllUserLunch.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // lblAllUserBreakfast
+            // 
+            lblAllUserBreakfast.BackColor = Color.LightSalmon;
+            lblAllUserBreakfast.Font = new Font("Verdana", 12F, FontStyle.Bold);
+            lblAllUserBreakfast.Location = new Point(164, 370);
+            lblAllUserBreakfast.Name = "lblAllUserBreakfast";
+            lblAllUserBreakfast.Size = new Size(209, 129);
+            lblAllUserBreakfast.TabIndex = 6;
+            lblAllUserBreakfast.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label10
+            // 
+            label10.BackColor = Color.SandyBrown;
+            label10.Font = new Font("Verdana", 12F, FontStyle.Bold);
+            label10.Location = new Point(164, 145);
+            label10.Name = "label10";
+            label10.Size = new Size(209, 40);
+            label10.TabIndex = 10;
+            label10.Text = "Kahvaltı";
+            label10.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label11
+            // 
+            label11.BackColor = Color.SandyBrown;
+            label11.Font = new Font("Verdana", 12F, FontStyle.Bold);
+            label11.Location = new Point(390, 145);
+            label11.Name = "label11";
+            label11.Size = new Size(209, 40);
+            label11.TabIndex = 11;
+            label11.Text = "Öğle Yemeği";
+            label11.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label12
+            // 
+            label12.BackColor = Color.SandyBrown;
+            label12.Font = new Font("Verdana", 12F, FontStyle.Bold);
+            label12.Location = new Point(844, 145);
+            label12.Name = "label12";
+            label12.Size = new Size(209, 40);
+            label12.TabIndex = 12;
+            label12.Text = "Ara Öğün";
+            label12.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // label13
+            // 
+            label13.BackColor = Color.SandyBrown;
+            label13.Font = new Font("Verdana", 12F, FontStyle.Bold);
+            label13.Location = new Point(616, 145);
+            label13.Name = "label13";
+            label13.Size = new Size(209, 40);
+            label13.TabIndex = 13;
+            label13.Text = "Akşam Yemeği";
+            label13.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // btnMothly
+            // 
+            btnMothly.BackColor = Color.DarkGray;
+            btnMothly.Font = new Font("Verdana", 12F, FontStyle.Bold);
+            btnMothly.Location = new Point(950, 535);
+            btnMothly.Name = "btnMothly";
+            btnMothly.Size = new Size(102, 38);
+            btnMothly.TabIndex = 14;
+            btnMothly.Text = "Aylık";
+            btnMothly.TextAlign = ContentAlignment.MiddleCenter;
+            btnMothly.Click += btnMothly_Click;
+            // 
+            // btnWeekly
+            // 
+            btnWeekly.BackColor = Color.DarkGray;
+            btnWeekly.Font = new Font("Verdana", 12F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            btnWeekly.Location = new Point(31, 535);
+            btnWeekly.Name = "btnWeekly";
+            btnWeekly.Size = new Size(102, 38);
+            btnWeekly.TabIndex = 15;
+            btnWeekly.Text = "Haftalık";
+            btnWeekly.TextAlign = ContentAlignment.MiddleCenter;
+            btnWeekly.Click += btnWeekly_Click_1;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.Image = Properties.Resources.top_5;
+            pictureBox1.Location = new Point(468, 519);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(131, 72);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 16;
+            pictureBox1.TabStop = false;
+            // 
+            // lstTop5
+            // 
+            lstTop5.FormattingEnabled = true;
+            lstTop5.ItemHeight = 15;
+            lstTop5.Location = new Point(31, 597);
+            lstTop5.Name = "lstTop5";
+            lstTop5.Size = new Size(1022, 319);
+            lstTop5.TabIndex = 17;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Verdana", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 162);
+            label2.Location = new Point(267, 41);
+            label2.Name = "label2";
+            label2.Size = new Size(607, 32);
+            label2.TabIndex = 18;
+            label2.Text = "Kullanıclıların En Çok Tükettiği Besinler";
+            // 
+            // Trends
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources.arasayfalar;
+            ClientSize = new Size(1064, 928);
+            Controls.Add(label2);
+            Controls.Add(lstTop5);
+            Controls.Add(pictureBox1);
+            Controls.Add(btnWeekly);
+            Controls.Add(btnMothly);
+            Controls.Add(label13);
+            Controls.Add(label12);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(lblAllUserSnack);
+            Controls.Add(lblAllUserDinner);
+            Controls.Add(lblAllUserLunch);
+            Controls.Add(lblAllUserBreakfast);
+            Controls.Add(lblCurrentUserSnack);
+            Controls.Add(lblCurrentUserDinner);
+            Controls.Add(lblCurrentUserLunch);
+            Controls.Add(lblCurrentUserBreakfast);
+            Controls.Add(label1);
+            Controls.Add(lblName);
+            Name = "Trends";
+            Text = "Trends";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblName;
+        private Label label1;
+        private Label lblCurrentUserBreakfast;
+        private Label lblCurrentUserLunch;
+        private Label lblCurrentUserDinner;
+        private Label lblCurrentUserSnack;
+        private Label lblAllUserSnack;
+        private Label lblAllUserDinner;
+        private Label lblAllUserLunch;
+        private Label lblAllUserBreakfast;
+        private Label label10;
+        private Label label11;
+        private Label label12;
+        private Label label13;
+        private Label btnMothly;
+        private Label btnWeekly;
+        private PictureBox pictureBox1;
+        private ListBox lstTop5;
+        private Label label2;
+    }
+}
---
+++ AvangersDietApp.UI/Trends.cs
@@ -0,0 +1,138 @@
+﻿using AvangersDietApp.DAL.Concrate;
+using AvangersDietApp.DAL.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AvangersDietApp.UI
+{
+    public partial class Trends : Form
+    {
+        AvangersContext db = new AvangersContext();
+        User currentUser = SessionManger.CurrentUser;
+
+        public Trends()
+        {
+            LoadData();
+            InitializeComponent();
+            ShowUserName();
+            Top5Food();
+        }
+
+
+
+        private void LoadData()
+        {
+            currentUser = (User)db.Users
+                .Include(u => u.UserMealFoods)
+                .ThenInclude(uf => uf.Meal)
+                .Include(u => u.UserMealFoods)
+                .ThenInclude(uf => uf.Food)
+                .FirstOrDefault(u => u.Id == currentUser.Id)!;
+        }
+
+        private void ShowUserName()
+        {
+            lblName.Text = currentUser.UserName;
+        }
+
+        private void Top5Food()
+        {
+            var foodList = db.UserMealFoods.Include(uf => uf.Food).GroupBy(x => x.FoodId).OrderByDescending(x => x.Count()).Take(5).Select(uf => new
+            {
+                Name = uf.FirstOrDefault()!.Food!.Name,
+                NumberOfFoods = uf.Count()
+            });
+
+            foreach (var food in foodList)
+            {
+                lstTop5.Items.Add(food);
+            }
+        }
+
+
+        private string FindFoodName(int foodId)
+        {
+            if (foodId == 0)
+                return "Bu hafta öğünlerin boş :)";
+            return db.Food.Where(x => x.Id == foodId).FirstOrDefault()!.Name;
+        }
+        private int FindFoodId(List<UserMealFood> ufList)
+        {
+            int maxCount = 0;
+            int count = 0;
+            int maxFoodId = 0;
+
+            var foodList = ufList.Select(uf => uf.FoodId).ToList();
+
+            foreach (int foodId in foodList.Distinct())
+            {
+                count = foodList.Count(f => f == foodId);
+
+                if (count > maxCount)
+                {
+                    maxCount = count;
+                    maxFoodId = foodId;
+                }
+            }
+
+            return maxFoodId;
+        }
+
+
+        private List<UserMealFood> FindUserMealFood(string mealName, int option, int reportType)
+        {
+            int dayNo = reportType == 0 ? -(int)DateTime.Now.DayOfWeek : -DateTime.Now.Day;
+
+            if (option == 0)
+            {
+                return currentUser.UserMealFoods.Where(x => x.Meal!.Name == mealName).Where(x => x.Meal!.MealTime > DateTime.Now.AddDays(dayNo) && x.Meal.MealTime <= DateTime.Now).ToList();
+            }
+            else
+            {
+                var ufs = new List<UserMealFood>();
+
+                foreach (User user in db.Users.Include(u => u.UserMealFoods).ThenInclude(uf => uf.Meal))
+                {
+                    ufs.AddRange(user.UserMealFoods.Where(x => x.Meal!.Name == mealName).Where(x => x.Meal!.MealTime > DateTime.Now.AddDays(dayNo) && x.Meal.MealTime.Day <= DateTime.Now.Day).ToList());
+                }
+
+                return ufs;
+            }
+        }
+        private void AllUserFoods(int reportType)
+        {
+            lblAllUserBreakfast.Text = FindFoodName(FindFoodId(FindUserMealFood("Kahvaltı", 1, reportType)));
+            lblAllUserLunch.Text = FindFoodName(FindFoodId(FindUserMealFood("Öğle Yemeği", 1, reportType)));
+            lblAllUserDinner.Text = FindFoodName(FindFoodId(FindUserMealFood("Akşam Yemeği", 1, reportType)));
+            lblAllUserSnack.Text = FindFoodName(FindFoodId(FindUserMealFood("Ara Öğün", 1, reportType)));
+        }
+
+        private void UserFoods(int reportType)
+        {
+            lblCurrentUserBreakfast.Text = FindFoodName(FindFoodId(FindUserMealFood("Kahvaltı", 0, reportType)));
+            lblCurrentUserLunch.Text = FindFoodName(FindFoodId(FindUserMealFood("Öğle Yemeği", 0, reportType)));
+            lblCurrentUserDinner.Text = FindFoodName(FindFoodId(FindUserMealFood("Akşam Yemeği", 0, reportType)));
+            lblCurrentUserSnack.Text = FindFoodName(FindFoodId(FindUserMealFood("Ara Öğün", 0, reportType)));
+        }
+
+        private void btnMothly_Click(object sender, EventArgs e)
+        {
+            UserFoods(1);
+            AllUserFoods(1);
+        }
+
+        private void btnWeekly_Click_1(object sender, EventArgs e)
+        {
+            UserFoods(0);
+            AllUserFoods(0);
+        }
+    }
+}
