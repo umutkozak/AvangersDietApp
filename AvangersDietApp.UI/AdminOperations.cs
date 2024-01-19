71260820e35113885695ee62d744e681065446ae
umutkozak <147056208+umutozak@users.noreply.github.com>
2024-01-19 12:13:43 +0300

    Last-One

--- AvangersDietApp.UI/AdminOperations.cs
+++ AvangersDietApp.UI/AdminOperations.cs
@@ -1,6 +1,7 @@
 ﻿using AvangersDietApp.BLL.Concrate;
 using AvangersDietApp.DAL.Concrate;
 using AvangersDietApp.DAL.Context;
+using Microsoft.Data.SqlClient;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -22,7 +23,7 @@ namespace AvangersDietApp.UI
 
         CategoryManager categoryManager = new CategoryManager();
         Category category = new Category();
-
+        SqlConnection baglan = new SqlConnection("server=.;database=AvangersDietDB;uid=sa;pwd=123;trustservercertificate=true;");
 
         private void btnFoodDelUpd_Click(object sender, EventArgs e)
         {
@@ -54,38 +55,73 @@ namespace AvangersDietApp.UI
 
         private void btn_CategoryUpd_Click(object sender, EventArgs e)
         {
-            int rowIndex = dataGridView1.SelectedCells[0].RowIndex;
-            var category = dataGridView1.Rows[rowIndex].Cells["UpddatedColumn"].Value;
+            //int rowIndex = dataGridView1.SelectedCells[0].RowIndex;
+            //var category = dataGridView1.Rows[rowIndex].Cells["UpddatedColumn"].Value;
+            //try
+            //{
+            //    categoryManager.Update((Category)category);
+            //    MessageBox.Show("İşlem Başarılı");
+            //}
+            //catch (Exception)
+            //{
+
+            //    MessageBox.Show("Beklenmedik Bir Hata Oluştu."); ;
+            //}--Null entity exception
             try
             {
-                categoryManager.Update((Category)category);
+                baglan.Open();
+                SqlCommand kmt = new SqlCommand("UPDATE Category SET Name = @Name, WHERE Id = @Id", baglan);
+                kmt.Parameters.AddWithValue("@Id", dataGridView1.CurrentRow.Cells["Id"].Value);
+                kmt.Parameters.AddWithValue("@Name", txt_Category.Text);
+                kmt.ExecuteNonQuery();
+                baglan.Close();
+                dataGridView1.DataSource = categoryManager.GetAll();
                 MessageBox.Show("İşlem Başarılı");
+
             }
             catch (Exception)
             {
 
-                MessageBox.Show("Beklenmedik Bir Hata Oluştu."); ;
+                MessageBox.Show("Beklenmedik Bir Hata Oluştu.");
             }
-            dataGridView1.DataSource = categoryManager.GetAll();
+
+
 
         }
 
         private void btn_CategoryDel_Click(object sender, EventArgs e)
         {
-            int selectedIndex = dataGridView1.SelectedRows[0].Index;
-            int rowID = Convert.ToInt32(dataGridView1.Rows[selectedIndex].Cells["ID"].Value);
+            //int selectedIndex = dataGridView1.SelectedRows[0].Index;
+            //int rowID = Convert.ToInt32(dataGridView1.Rows[selectedIndex].Cells["ID"].Value);
 
-            if (category.Id==rowID)
+            //if (category.Id==rowID)
+            //{
+            //    categoryManager.Delete(category);
+            //    MessageBox.Show("İşlem Başarılı");
+            //}
+            //else
+            //{
+            //    MessageBox.Show("İşlem sırasında bir hata oluştu.");
+            //}----Null entity exception
+
+            try
             {
-                categoryManager.Delete(category);
-                MessageBox.Show("İşlem Başarılı");
+                baglan.Open();
+                SqlCommand kmt = new SqlCommand("DELETE FROM Category WHERE Name = @Name", baglan);
+                kmt.Parameters.AddWithValue("@Name", txt_Category.Text);
+                kmt.ExecuteNonQuery();
+                baglan.Close();
+                dataGridView1.DataSource = categoryManager.GetAll();
+                MessageBox.Show("Kayıt Silindi");
+
+
             }
-            else
+            catch (Exception)
             {
-                MessageBox.Show("İşlem sırasında bir hata oluştu.");
+
+                MessageBox.Show("Beklenmedik bir hata oluştu");
             }
 
-            dataGridView1.DataSource = categoryManager.GetAll();
 
         }
 
@@ -145,11 +181,16 @@ namespace AvangersDietApp.UI
 
         private void btn_Reports_Click(object sender, EventArgs e)
         {
-            Register register = new Register();
-            register.Show();
+            Trends trends = new Trends();
+            trends.Show();
             this.Hide();
         }
 
-     
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txt_Category.Text=dataGridView1.CurrentRow.Cells[0].Value.ToString();
+        }
+
+
     }
 }
